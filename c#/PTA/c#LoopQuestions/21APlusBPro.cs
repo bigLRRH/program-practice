9fc8a866736650bf8ed6bb1a137ad11f3f954dfc
bigLRRH <51252247+bigLRRH@users.noreply.github.com>
2021-10-08 19:16:35 +0800

    routine update

--- c#/PTA/c#LoopQuestions/21APlusBPro.cs
+++ c#/PTA/c#LoopQuestions/21APlusBPro.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 /*
 7-21 A+B (10 point(s))
@@ -21,10 +24,9 @@ Output:
 namespace LoopQuestioons{
     class APlusB{
         static void Main(string[] args){
-            int a = int.Parse(Console.ReadLine());
-            int b = int.Parse(Console.ReadLine());
+            int[] a=Array.ConvertAll<string, int>(Console.ReadLine().Split(' '), int.Parse);
 
-            long c = a+b;
+            long c = a[0]+a[1];
             Console.WriteLine(c);
         }
     }
