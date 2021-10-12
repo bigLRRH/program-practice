f0c112eff7c0ae9d9c789b418ed9041024919cee
bigLRRH <51252247+bigLRRH@users.noreply.github.com>
2021-10-12 13:32:27 +0800

    optimize the code

--- c++/AlgorithmClub/EnterTest/2p1116_1.cpp
+++ c++/AlgorithmClub/EnterTest/2p1116_1.cpp
@@ -7,10 +7,10 @@ int main(){
     int N;cin >> N;
     int a[N];
     for(int i=0;i<N;i++)cin >> a[i];
-    int output = 0;
+    int output = i-i-
 
     for(int i=0;i<N-1;i++){
-        for(int j=0;j<N-1;j++){
+        for(int j=0;j<N-i-1;j++){
             if(a[j]>a[j+1]){
                 swap(a[j],a[j+1]);
                 output++;
